24888c21088d1bde634456a4e6c8f4e03fb762c6
Joaco Casa <joaquintorres@studio4d.com>
2025-03-18 15:56:49 -0300

    Refactor code formatting for consistency and readability

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,18 @@ interface Author {
 }
 
 interface Book {
-  title: string;
-  pages: number;
-  genre: string;
-  cover: string;
-  synopsis: string;
-  year: number;
-  ISBN: string;
-  author: Author;
+    title: string;
+    pages: number;
+    genre: string;
+    cover: string;
+    synopsis: string;
+    year: number;
+    ISBN: string;
+    author: Author;
 }
 
 interface BookItem {
-  book: Book;
+    book: Book;
 }
 
 const app = express();
@@ -28,20 +28,20 @@ let bookItems: BookItem[] = [];
 
 // Function to fetch the books data from the JSON file
 async function fetchBooksData(): Promise<void> {
-  try {
-    const data = await fs.readFile('./data.json', 'utf8');
-    const lib = JSON.parse(data)
-    bookItems = lib.library;
-    console.log('Books data loaded.');
+    try {
+        const data = await fs.readFile('./data.json', 'utf8');
+        const lib = JSON.parse(data)
+        bookItems = lib.library;
+        console.log('Books data loaded.');
     } catch (error) {
-    console.error('Error loading data:', error);
+        console.error('Error loading data:', error);
   }
 }
 
 // Endpoint to get all the books
 app.get('/books', (req: Request, res: Response) => {
-  const books = bookItems.map(item => item.book);
-  res.json(books);
+    const books = bookItems.map(item => item.book);
+    res.json(books);
 });
 
 // Endpoint to get books by author
@@ -49,36 +49,36 @@ app.get('/books/author', (req: Request, res: Response) => {
     const { search } = req.query;
   
     if (!search || typeof search !== 'string') {
-      return res.status(400).json({ error: 'A valid author search parameter is required.' });
+        return res.status(400).json({ error: 'A valid author search parameter is required.' });
     }
   
     const filteredBooks = bookItems
-      .filter(item => item.book.author.name.toLowerCase() === search.toLowerCase())
-      .map(item => item.book);
+        .filter(item => item.book.author.name.toLowerCase() === search.toLowerCase())
+        .map(item => item.book);
   
     if (filteredBooks.length > 0) {
-      res.json(filteredBooks);
+        res.json(filteredBooks);
     } else {
-      res.status(404).json({ error: 'No books found for the specified author.' });
+        res.status(404).json({ error: 'No books found for the specified author.' });
     }
 });
 
 // Endpoint to get a book by ISBN
 app.get('/books/:isbn', (req: Request, res: Response) => {
-  const { isbn } = req.params;
-  const found = bookItems.find(item => item.book.ISBN === isbn);
-  
-  if (found) {
-    res.json(found.book);
-  } else {
-    res.status(404).json({ error: 'Book not found' });
-  }
+    const { isbn } = req.params;
+    const found = bookItems.find(item => item.book.ISBN === isbn);
+    
+    if (found) {
+        res.json(found.book);
+    } else {
+        res.status(404).json({ error: 'Book not found' });
+    }
 });
 
   
 
 // Start the server and load the books data
 app.listen(PORT, async () => {
-  console.log(`Server Running on http://localhost:${PORT}`);
-  await fetchBooksData();
+    console.log(`Server Running on http://localhost:${PORT}`);
+    await fetchBooksData();
 });
